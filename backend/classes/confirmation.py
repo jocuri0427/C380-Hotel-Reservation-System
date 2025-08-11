6026bb5ae4b0f12505464727909d1eaade5af973
jaypatel17486 <jay.171124@icloud.com>
2025-08-11 14:35:12 -0700

    Add files via upload

---
+++ backend/classes/confirmation.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+
+class Confirmation:
+    def __init__(self, confirmation_number, booking_details):
+        self.confirmation_number = confirmation_number
+        self.booking_details = booking_details
+
+    def get_summary(self):
+        return self.confirmation_number + "\n" + self.booking_details
+
+    def gen_confirmation_number():
+        return "CON-" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+
+
