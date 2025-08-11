5082ce6d9cda6cc066448dacd13b9760229dc043
jaypatel17486 <jay.171124@icloud.com>
2025-08-11 14:35:00 -0700

    Delete backend/classes/confirmation.py

--- backend/classes/confirmation.py
+++
@@ -1,7 +0,0 @@
-class Confirmation:
-    def __init__(self, confirmation_number, booking_details):
-        self.confirmation_number = confirmation_number
-        self.booking_details = booking_details
-
-    def get_summary(self):
-        return self.confirmation_number + "\n" + self.booking_details
